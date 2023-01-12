f96c0847710640add306acd88dfc9b78bb332ea4
Blow07 <boyemouhamed43@gmail.com>
2023-01-12 20:28:19 +0100

    Update New_protov1.py

--- New_protov1.py
+++ New_protov1.py
@@ -4,10 +4,10 @@ Created on Fri Jan  6 23:17:51 2023
 
 @author: boyem
 """
+import matplotlib.pyplot as plt
 
-fichier=open("Dumpfile.txt","r")
 
-
+fichier=open("Dumpfiletest.txt","r")
 texte=fichier.readlines()
 fichier.close()
 first_len=len(texte)
@@ -17,21 +17,12 @@ for _ in range(first_len):
         if texte[i].startswith("\t"):
             del texte[i]
             break
+        
 longueur=len(texte)
-print("hello")
-    
 
 
 resultat = [[""] * 1 for _ in range(longueur) ]
-
-def cherchepour(chaine):
-    global valeur,tab_of_event
-    for event in texte:
-        if event.startswith(chaine):
-            tab_of_event=event.split(":")
-            valeur=tab_of_event[1]
-            break
-    return valeur
+print(resultat)
 
 
 colonne=["TIME","IPSRC","IPDST","FLAGS","SEQ","ACK",
@@ -55,7 +46,7 @@ for i in range(longueur):
 
 print(resultat)
 
-f = open('TheFichier.csv', 'w')
+f = open('TheFichiertest.csv', 'w')
 ligneEntete = ";".join(colonne) + "\n"
 f.write(ligneEntete)
 for valeur in resultat:
@@ -65,26 +56,46 @@ f.close()
 
 
 print(f"{longueur=}")
-"""
-test=texte[0].split()
-to_remove=[1,2,3,4,5,6,7,12]
-to_fuse=[7,7,7,7,7] 
 
 
-for element in to_remove:
-    del test[element]
-    print(test)
-    print("\n")
-
-element_to_fuse=[]
-for i in to_fuse:
-    element_to_fuse.append(test.pop(i))
-test.insert(-1,"-".join(element_to_fuse))
-print(test)
-"""
+chaine_adresse=[]
+resultat_adresse_tout=[]
+resultat_adresse_nombre=[]
+chaine_traite=[]
 
 
+for i in range(len(resultat)):
+    
+    if resultat[i][1].startswith("BP"):
+        a,b=resultat[i][1].split("."),resultat[i][2].split(".")
+        chaine= a[0] + "==>" + b[0]+b[1]+b[2]+b[3]
+        
+        
+        
+    elif resultat[i][2].startswith("BP"):
+        a,b=resultat[i][1].split("."),resultat[i][2].split(".")
+        chaine= a[0]+a[1]+a[2]+a[3] + "==>" + b[0]
+    
+    
+    
+    resultat_adresse_tout.append(chaine)
+    if chaine not in chaine_adresse:
+        chaine_adresse.append(chaine)
+        
+        
+for element in chaine_adresse:
+    if element not in chaine_traite:
+        resultat_adresse_nombre.append(str(resultat_adresse_tout.count(element)))
+        chaine_traite.append(element)
+        
+        
+        
+print(resultat_adresse_nombre)
+        
 
-
-
-
+f = open('Donnes.csv', 'w')
+ligneEntete = ";".join(chaine_adresse) + "\n"
+f.write(ligneEntete)
+ligneEntete = ";".join(resultat_adresse_nombre)
+f.write(ligneEntete)
+f.close()
