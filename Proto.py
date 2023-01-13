80a6abe043829dc149cf0d2ce0c959a697121e4a
Blow07 <boyemouhamed43@gmail.com>
2023-01-13 10:22:34 +0100

    Update Proto.py

--- Proto.py
+++ Proto.py
@@ -4,8 +4,8 @@ Created on Thu Dec 15 10:28:55 2022
 
 @author: boyem
 """
-import datetime
-import numpy as np
+
+import matplotlib.pyplot as plt
 fichier=open("Calendar.txt","r")
 texte=fichier.read()
 tableau=texte.split("\n")
@@ -16,7 +16,8 @@ tableau=texte.split("\n")
 nb_event=tableau.count("BEGIN:VEVENT")
 resultat = [[0] * 10 for _ in range(nb_event) ]
 
-
+nb_r107=tableau.count("SUMMARY:R107")
+tab_r107 = [[0] * 10 for _ in range(nb_r107-2) ]
 
 
 def cherchepour(chaine):
@@ -66,3 +67,48 @@ for valeur in resultat:
      ligne = ";".join(valeur) + "\n"
      f.write(ligne)
 f.close() 
+
+for i in range(len(resultat)):
+    if resultat[i][3]=="R107":
+        tab_r107[j]=resultat[i]
+        j+=1
+  
+   
+
+
+f = open('r107.csv', 'w')
+ligneEntete = ";".join(chaine) + "\n"
+f.write(ligneEntete)
+for valeur in tab_r107:
+     ligne = ";".join(valeur) + "\n"
+     f.write(ligne)
+f.close() 
+
+
+nb_tp_dec=0
+nb_tp_nov=0
+nb_tp_oct=0
+nb_tp_sept=0
+for i in range(len(resultat)):
+    if ("202112" in resultat[i][1])==True and ("RT1-TP" in resultat[i][5])==True:
+        nb_tp_dec+=1
+    elif ("202111" in resultat[i][1])==True and ("RT1-TP" in resultat[i][5])==True:
+        nb_tp_nov+=1
+    elif ("202110" in resultat[i][1])==True and ("RT1-TP" in resultat[i][5])==True:
+        nb_tp_oct+=1
+    elif ("202109" in resultat[i][1])==True and ("RT1-TP" in resultat[i][5])==True:
+        nb_tp_sept+=1
+        
+        
+        
+        
+
+
+labels = ["Septembre", "Octobre", "Novembre", "Décembre"]
+sizes = [nb_tp_sept, nb_tp_oct, nb_tp_nov, nb_tp_dec]
+colors = ["yellowgreen", "gold", "lightskyblue", "lightcoral"]
+plt.pie(sizes, labels=labels, colors=colors,
+autopct="%1.1f%%", shadow=True, startangle=90)
+plt.axis("equal")
+plt.savefig("PieChart01.png")
+plt.show()
